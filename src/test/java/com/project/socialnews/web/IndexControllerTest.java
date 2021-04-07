de81b9a30a0d08188f06949260bada3faba2a09b
proost <jwalag87@gmail.com>
2021-04-08 01:32:13 +0900

    add main page

---
+++ src/test/java/com/project/socialnews/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.project.socialnews.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadMain() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("Social News");
+    }
+}
